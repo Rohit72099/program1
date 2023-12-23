ccd3d1301d8576e44044ff3e12706b5bcb749df0
Rohit72099 <112176838+Rohit72099@users.noreply.github.com>
2023-12-23 22:41:22 +0530

    Add files via upload

---
+++ start_java/src/start_java/program1.java
@@ -0,0 +1,28 @@
+
+package start_java;
+//import Java.util.Scanner
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class program1 {
+
+	public static void main(String[] args) {
+		java.util.Scanner read = new Scanner(System.in);
+		int t = read.nextInt();
+		for(int i= 0;i<t;i++) {
+//			System.out.println(i);
+			String s =read.next();
+			String x= s+s;
+//			System.out.println(x);
+		}
+		System.out.println(x);
+		
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+
+	}
+
+}
+
